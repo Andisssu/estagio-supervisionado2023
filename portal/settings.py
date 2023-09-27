59aacc81af3d82e41608a619be7f6e13d3c27682
Anderson Dantas Santos <netodantas3@gmail.com>
2023-09-27 08:24:11 -0500

    updates icone sidebar

--- portal/settings.py
+++ portal/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/templates/static/'
 STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static') #templates/static
